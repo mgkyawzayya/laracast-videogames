d2c4cc8d1a6e95764d52bdf15e2cf5da90e8e22d
Kyaw Zayya <kyawzayya.ucsp@gmail.com>
2020-11-30 11:25:41 +0630

    Complete

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laracasts Video Games</title>
+    <link rel="stylesheet" href="/css/main.css">
+</head>
+
+<body class="bg-gray-900 text-white">
+    <header class="border-b border-gray-800">
+        <nav class="container mx-auto flex items-center justify-between px-4 py-6">
+            <div class="flex items-center">
+                <a href="/">
+                    <img src="/logo.svg" alt="laracasts" class="w-32 flex-none">
+                </a>
+                <ul class="flex ml-16 space-x-8">
+                    <li>
+                        <a href="/" class="hover:text-gray-400">Games</a>
+                        <a href="/reviews" class="hover:text-gray-400">Reviews</a>
+                        <a href="/comming-soon" class="hover:text-gray-400">Coming Soon</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="flex items-center">
+                <div class="relative">
+                    <input type="text" class="bg-gray-800 text-sm rounded-full focus:outline-none focus:shadow-outlline w-64 px-3 py-1" placeholder="Search ...">
+                </div>
+                <div class="ml-6">
+                    <a href="#"><img src="/avatar.jpg" alt="avatar" class="rounded-full w-8"></a>
+                </div>
+            </div>
+        </nav>
+    </header>
+</body>
+
+</html>
